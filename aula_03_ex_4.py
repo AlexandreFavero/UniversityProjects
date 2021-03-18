ffb9291c2662833712287690618e6325274ec8a5
AlexandreFavero <80922249+AlexandreFavero@users.noreply.github.com>
2021-03-18 17:20:13 -0300

    Add files via upload
    Given the larger base, the smaller base and the height of a trapezoid, it calculates and displays the value of its area.
    Dada  a  base  maior,  a  base  menor  e  a  altura  de  um  trapézio,  calcula e exibe o valor da sua área.

---
+++ aula_03_ex_4.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar  4 15:11:47 2021
+
+@author: Alexandre Del Fávero
+"""
+B=float(input("Digite o valor da base maior:"))
+b=float(input("Digite o valor da base menor:"))
+H=float(input("Digite o valor da altura: "))
+A=((B+b)*H/2)
+print("A area é %.3f" %(A))
