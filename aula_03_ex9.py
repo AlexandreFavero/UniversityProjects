0d2a3fab39f461ed1d9b25505dfdc61c2fa27c54
AlexandreFavero <80922249+AlexandreFavero@users.noreply.github.com>
2021-03-18 17:28:34 -0300

    Add files via upload
    Converts from radians to degrees

---
+++ aula_03_ex9.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar  4 15:55:10 2021
+
+@author: Alexandre Del Fávero
+"""
+unidade= "radianos"
+ag=float(input("digite o ângulo em graus a ser convertido para radianos:"))
+ar=ag*3.1415926536/180
+print("O valo é", ar, unidade)
