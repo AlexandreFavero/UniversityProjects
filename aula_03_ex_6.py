e3fb582b5560de783300a6b0b1a004b8d9cfb410
AlexandreFavero <80922249+AlexandreFavero@users.noreply.github.com>
2021-03-18 17:25:12 -0300

    Add files via upload
    Transforms a given temperature in ° C to the corresponding temperature in ° F
    Transforma  uma  temperatura  fornecida  em °C  para  o correspondente em °F

---
+++ aula_03_ex_6.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar  4 15:31:11 2021
+
+@author: Alexandre Del Fávero
+"""
+
+c=float(input("Digite a temperatura em °C para ser convertida em °F:"))
+f=c*9/5+32
+print("A temperatura é %.2f°f."%(f))
